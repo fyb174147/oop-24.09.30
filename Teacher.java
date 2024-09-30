033f574136baa4817dcefeda4b7daad82d37e02b
fyb174147 <fyb174146@gmail.com>
2024-09-30 16:43:49 +0700

    add class Teacher

---
+++ Teacher.java
@@ -0,0 +1,9 @@
+public class Teacher extends Person{
+    public Teacher(){
+        super();
+    }
+
+    public Teacher(String name, int yearOfBirth){
+        super(name, yearOfBirth);
+    }
+}
