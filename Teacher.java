c641ba4ef2c2ca04d7c82c2326ec57e5c74cf263
fyb174147 <fyb174146@gmail.com>
2024-10-07 14:39:00 +0700

    fix class Teacher

--- Teacher.java
+++ Teacher.java
@@ -1,9 +1,21 @@
 public class Teacher extends Person{
+    private String department;
+
     public Teacher(){
         super();
+        this.department = "";
     }
 
-    public Teacher(String name, int yearOfBirth){
+    public Teacher(String name, int yearOfBirth, String department){
         super(name, yearOfBirth);
+        this.department = department;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
     }
 }
